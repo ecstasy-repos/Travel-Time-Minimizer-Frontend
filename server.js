2e5281a6eb74136d574b4496b47f5b29921fff4e
kz-parmanand <parmanand@kwanzoo.com>
2021-07-22 08:06:21 +0530

    added cors in express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+import cors from 'cors';
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
 
+app.use(cors());
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
